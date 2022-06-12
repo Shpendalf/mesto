14122afcc0b8e015d94e4b69e25559bffebdbb5f
nikolay zheltov <shpendalf@gmail.com>
2022-06-12 15:41:30 +0300

    mestofix4

--- script/index.js
+++ script/index.js
@@ -22,38 +22,38 @@ const openButton = document.querySelector('.profile__edit-button')
 const popupClass = document.querySelector('.popup')
 const closeButton = document.querySelector('.popup__close')
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector ('.popup__textarea_name');
+const nameInput = document.querySelector('.popup__textarea_type_name');
 const profName = document.querySelector('.profile__name');
-const jobInput = document.querySelector('.popup__textarea_status')
+const jobInput = document.querySelector('.popup__textarea_type_status')
 const profStatus = document.querySelector('.profile__status')
 
 
 
-function openPopup(event){
-      popupClass.classList.add('popup_open')
-      nameInput.value=''
-      jobInput.value=''
-      nameInput.value=profName.textContent;
-      jobInput.value=profStatus.textContent;
-      
-    }
-function closePopup(){
-    popupClass.classList.remove('popup_open')
-    }
+function openPopup(event) {
+  popupClass.classList.add('popup_open')
+  nameInput.value = ''
+  jobInput.value = ''
+  nameInput.value = profName.textContent;
+  jobInput.value = profStatus.textContent;
 
-
-
-function textChange(evt){
-    evt.preventDefault();
-    
-    profName.textContent=nameInput.value;
-    profStatus.textContent=jobInput.value;
-  
-    profName.value="";
-    profStatus.value=""   
-    popupClass.classList.remove('popup_open')
 }
-formElement.addEventListener('submit',textChange)
+function closePopup() {
+  popupClass.classList.remove('popup_open')
+}
+
+
+
+function textChange(evt) {
+  evt.preventDefault();
+
+  profName.textContent = nameInput.value;
+  profStatus.textContent = jobInput.value;
+
+  profName.value = "";
+  profStatus.value = ""
+  popupClass.classList.remove('popup_open')
+}
+formElement.addEventListener('submit', textChange)
 
 
 openButton.addEventListener('click', openPopup)
